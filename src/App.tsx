c3869e601b9f22c89e5322c9529ba9c6600000b5
David Alejandro Luna <david.lunamar@campusucc.edu.co>
2025-11-05 21:12:53 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/Navbar";
 
-const App: React.FC = () => {
+function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1 p-6">
-          <AppRoutes />
-        </main>
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   );
-};
+}
 
 export default App;
