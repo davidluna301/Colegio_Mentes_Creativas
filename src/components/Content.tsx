f6b9d0db20443f1dfb197b9540222ee2ee77ac94
davidluna301 <david.lunamar@campusucc.edu.co>
2025-11-09 20:59:44 +0000

    Correciones menores en Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Content() {
   return (
-    <div className="h-full w-full">
+    <div> className="h-full w-full"
       {/* Aquí React Router renderiza el componente de la ruta activa */}
       <Outlet />
     </div>
